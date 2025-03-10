afd1a8013ecbaf008051dce7d6a3d787339c4f43
MajitelAfriky <84845036+MajitelAfriky@users.noreply.github.com>
2025-03-10 12:11:40 +0100

    Update rpi.ino

--- rpi.ino
+++ rpi.ino
@@ -7,26 +7,27 @@
 #define AXI_Z 26
 const int btn[] = {0, 0, 1, 17, 14};
 const int btn1[] = {0, 10, 11, 12, 13, 6, 7, 8, 9};
-int btnState[] = {0, 0, 0, 0, 0};
-int btnState1[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int btnState[4];// = {0, 0, 0, 0, 0};
+int btnState1[9];// = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 const int lght[] = {0, 0, 0, 0, 15};
 const int lght1[] = {0, 2, 3, 4, 5, 21, 20, 19, 18};
-unsigned long preMil[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-unsigned long blinkInt[] = {0, 250, 250, 250, 250, 0, 0, 0, 0};
-bool ledState[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long preMil[9];// = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int blinkVal[10];// = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int blinkInt[10];// = {0, 250, 250, 250, 250, 0, 0, 0, 0};
+bool ledState[9];// = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 bool light = true;
 Adafruit_NeoPixel pixels(NEO_PIX, NEO, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(115200);
-  for (int i = 1; i < 5; i++) {
+  Serial.begin(9600);
+  for (int i = 1; i <= 4; i++) {
     pinMode(btn[i], INPUT_PULLUP);
   } 
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 8; i++) {
     pinMode(btn1[i], INPUT_PULLUP);
   } 
   pinMode(lght[4], OUTPUT);   
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 8; i++) {
     pinMode(lght1[i], OUTPUT);
   }  
   Joystick.begin();
@@ -34,16 +35,61 @@ void setup() {
 }
 
 void loop() {
-  if (light == true) {intro();}
+if (light == true) {intro();}
+  dmx();
   joystick();
   heat();
+}
+void dmx() {
+
+  if (Serial.available() > 0) {
+    String receivedData = Serial.readStringUntil('\n');
+    char buffer[50];  // Dočasné pole pro převedení Stringu na C-string
+    receivedData.toCharArray(buffer, sizeof(buffer)); //                 reaktor       paprsky       torpedo        manev        impuls         warp           jump          front         rear
+    int parsedValues = sscanf(buffer, "%d %d %d %d %d %d %d %d %d %d", &blinkVal[1], &blinkVal[2], &blinkVal[3], &blinkVal[4], &blinkVal[5], &blinkVal[6], &blinkVal[20], &blinkVal[7], &blinkVal[8], &blinkVal[9]);
+  } else {
+    for (int i = 1; i <= 9; i++) {
+        blinkVal[i] = 0;
+    }
+    blinkVal[20] = 0;
+  }
+  if (blinkVal[1] > 170) {
+    for (int i = 1; i <= 9; i++) {
+      blinkInt[i] = 80;
+    }
+    } else {
+      for (int i = 2; i <= 9; i++) {
+        if (blinkVal[i] > 60) {
+          blinkInt[i] = map(blinkVal[i],60, 255,500,80);
+        } else if (blinkVal[i] > 50) {
+          blinkInt[i] = 500;
+        } else {
+          blinkInt[i] = 0;
+        }
+      }
+      if (blinkVal[20] > 60) {
+        blinkInt[20] = map(blinkVal[20],60, 255,500,80);
+      } else if (blinkVal[20] > 50) {
+        blinkInt[20] = 500;
+      } else {
+        blinkInt[20] = 0;
+      }
+      if (blinkVal[1] > 60) {
+        blinkInt[1] = map(blinkVal[1],60, 170,500,80);
+      } else if (blinkVal[1] > 50) {
+        blinkInt[1] = 500;
+      } else {
+        blinkInt[1] = 0;
+      }
+      blinkInt[6] = blinkInt[6] + blinkInt[20];
+  }
 }
 
 void heat() {
 
   unsigned long currentMillis = millis();
 
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 9; i++) {
     if (blinkInt[i] > 0) {
       if (currentMillis - preMil[i] >= blinkInt[i]) {
       preMil[i] = currentMillis;
@@ -58,16 +104,16 @@ void heat() {
 }
 
 void joystick() {
-  for (int i = 1; i < 5; i++) {
+  for (int i = 1; i <= 4; i++) {
     btnState[i] = digitalRead(btn[i]);
   }
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 8; i++) {
     btnState1[i] = digitalRead(btn1[i]);
   }
-  for (int i = 1; i < 5; i++) {
+  for (int i = 1; i <= 4; i++) {
     if (btnState[i] == LOW)   {button(i, true);}  else {button(i, false);}
   }
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 8; i++) {
     if (btnState1[i] == LOW)  {button(i, true);} else {button(i, false);}
   }
   
@@ -79,9 +125,6 @@ void joystick() {
   }
   //value_x = constrain(value_x, 0, 1023);
   Joystick.X(value_x);
-  Serial.print(value_x);
-  Serial.print("   |   ");
-
   int value_y;
   if (analogRead(AXI_Y) >= 280) {
     value_y = map(analogRead(AXI_Y), 280, 1023, 511, 0);
@@ -90,8 +133,7 @@ void joystick() {
   }
   //value_y = constrain(value_y, 0, 1023);
   Joystick.Y(value_y);
-  Serial.print(value_y);
-  Serial.print("   |   ");
+
 
   int value_z;
   if (analogRead(AXI_Z) >= 280) {
@@ -101,7 +143,6 @@ void joystick() {
   }
   //value_z = constrain(value_z, 0, 1023);
   Joystick.Z(value_z);
-  Serial.println(value_z);
 
 }
 
@@ -110,7 +151,7 @@ void button(int n, bool tf) {
 }
 
 void intro() {
-  for (int i = 1; i < 9; i++) {
+  for (int i = 1; i <= 8; i++) {
     digitalWrite(lght1[i], HIGH);
     delay(60);
     digitalWrite(lght1[i], LOW);
